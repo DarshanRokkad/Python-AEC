7b64bf43129fcd92ea77232cc58a88c6cd5f26f2
Darshan R.M <90130734+darshancpu@users.noreply.github.com>
2023-07-12 19:04:42 +0530

    modified program no 9

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
 def convert_centigrade_to_fahrenheit(c):
-	return (float)(c*9/5)+32
+    return (float)(c*9/5)+32
 def convert_fahrenheit_to_centigrade(f):
-	return (float)(f-32)*5/9
+    return (float)(f-32)*5/9
